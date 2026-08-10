32a1003fbbb4108aad6108bb12f04cc12ce1efb9
Asthil <zodoxe@gmail.com>
2026-08-10 21:26:58 +0200

    Setup CMake + vcpkg + SFML, fenetre de base fonctionnelle

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <SFML/Graphics.hpp>
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "Etincelle");
+
+    while (window.isOpen()) {
+        while (const std::optional event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.clear(sf::Color::Black);
+        window.display();
+    }
+
+    return 0;
+}
