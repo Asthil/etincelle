8f5b18f44cdf3646d68f63e75d4543703fa95ae2
Asthil <zodoxe@gmail.com>
2026-08-24 10:26:18 +0200

    Supression du jeu pour repartir sur des bases saines

--- src/Game/InputHandler.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <SFML/Graphics.hpp>
-#include <SFML/Window.hpp>
-#include <iostream>
-
-void InputHandler(const sf::Event& event, sf::RenderWindow& window);
--- src/main.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <SFML/Graphics.hpp>
-#include <SFML/Window.hpp>
-#include <iostream>
-
-const int WIN_WIDTH = 1200;
-const int WIN_HEIGHT = 1200;
